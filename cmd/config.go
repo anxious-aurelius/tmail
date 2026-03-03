cd223b04e62b82b34a65282bcf7e999bb80e0975
Kripal Parsekar <kripalparsekar@gmail.com>
2026-03-03 23:22:03 +0000

    added toml file handling package

--- cmd/config.go
+++ cmd/config.go
@@ -6,10 +6,10 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/anxious-aurelius/tmail/config"
 	"github.com/spf13/cobra"
 )
 
-// configCmd represents the config command
 var configCmd = &cobra.Command{
 	Use:   "config",
 	Short: "A brief description of your command",
@@ -21,19 +21,10 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("config command - not yet implemented")
+		config.Load()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(configCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// configCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// configCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
