120a15b7b1380a510c3e39deed8e4b2715c18365
kr19al0711 <kripalparsekar@gmail.com>
2026-03-08 22:55:12 +0000

    added imap envelope struct and added few comments

--- internal/mail/mail.go
+++ internal/mail/mail.go
@@ -1,7 +1,34 @@
 package mail
 
+import "time"
+
 type Message struct {
-	To      []string
+	// Recipient Emails
+	To []string
+	// Mail Subject
 	Subject string
-	Body    string
+	// Mail Body
+	Body string
+}
+
+// Envelop struct taken from emersion/imap-client
+type Envelope struct {
+	// The message date.
+	Date time.Time
+	// The message subject.
+	Subject string
+	// The From header addresses.
+	From []*Address
+	// The message senders.
+	// Mail flags
+	Flags []string
+}
+
+type Address struct {
+	// The personal name.
+	PersonalName string
+	// The mailbox name.
+	MailboxName string
+	// The host name.
+	HostName string
 }
