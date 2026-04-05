1f52944996cd9b3d19a3c8b30d276ef2d6fe7496
Kripal Parsekar <kripalparsekar@gmail.com>
2026-04-05 16:42:15 +0100

    listing n latest emails

--- internal/imap/imap.go
+++ internal/imap/imap.go
@@ -1,38 +1,64 @@
 package imap
 
 import (
+	"fmt"
 	"log"
 	"strconv"
 
 	"github.com/anxious-aurelius/tmail/config"
 	"github.com/anxious-aurelius/tmail/internal/mail"
+	"github.com/emersion/go-imap"
 	"github.com/emersion/go-imap/client"
 )
 
 // TLS Handshake -> Login -> Select Folder -> Fetch Messages -> Logout and close connection
 func ListEvelopes(n int, cfg *config.Config) ([]mail.Envelope, error) {
+
 	// TLS handshake with IMAP
 	log.Println("Connecting to the server.")
-	conn, err := client.Dial(cfg.ImapConfig.Host + ":" + strconv.Itoa(cfg.ImapConfig.Port))
+	conn, err := client.DialTLS(cfg.ImapConfig.Host+":"+strconv.Itoa(cfg.ImapConfig.Port), nil)
+
 	if err != nil {
-		return nil, err
+		fmt.Println("Couldn't connect to the IMAP server")
 	}
+	fmt.Println("Connection succesfully")
 
-	log.Println("Connected to IMAP")
-
-	// Login
+	fmt.Println("Logging in...")
 	if err = conn.Login(cfg.ImapConfig.Username, cfg.ImapConfig.Password); err != nil {
-		return nil, err
+		fmt.Println("Error when logging in")
 	}
+	fmt.Println("Successfully logged in")
 
-	defer conn.Logout()
+	fmt.Println("Fetching mails from INBOX")
 
-	mbox, err := conn.Select("INBOX", true)
+	//select mailbox
+	selectedMB, err := conn.Select("INBOX", true)
 	if err != nil {
-		return nil, err
+		fmt.Println("Error selecting mailbox")
 	}
 
-	log.Println("Flags for INBOX:", mbox.Flags)
+	// if mailbox size is greater than the requested list size. just return the last n envelopes
+	seqFrom := uint32(1)
+	seqTo := selectedMB.Messages
 
+	if selectedMB.Messages > uint32(n) {
+		seqFrom = selectedMB.Messages - uint32(n)
+	}
+
+	seqset := new(imap.SeqSet)
+	seqset.AddRange(seqFrom, seqTo)
+
+	message := make(chan *imap.Message, n)
+	done := make(chan error, 1)
+
+	go func() {
+		done <- conn.Fetch(seqset, []imap.FetchItem{imap.FetchEnvelope}, message)
+	}()
+
+	log.Println("Fetching mails from the mail server.")
+
+	for msg := range message {
+		fmt.Println("* " + msg.Envelope.Subject)
+	}
 	return nil, nil
 }
