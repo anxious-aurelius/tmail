bd608d3c56b49787fe2ab6bf7f465c22f1441432
Kripal Parsekar <110184961+anxious-aurelius@users.noreply.github.com>
2026-06-13 12:07:50 +0100

    the configs are now fetched from the ~/.tmail/gonfig.toml (#22)

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,11 @@
 package config
 
 import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/BurntSushi/toml"
 )
 
@@ -21,13 +26,31 @@ type Imap struct {
 	Password string `toml:"password"`
 }
 
+// Loads config into memory
 func Load() (*Config, error) {
 	var config Config
-	//TODO: need to change the path to home directory once packaged into a executable
-	var path = "config.toml"
-	_, err := toml.DecodeFile(path, &config)
+	var ErrConfigNotFound = errors.New("config file not found")
+
+	homeDir, err := os.UserHomeDir()
+
 	if err != nil {
 		return nil, err
+	}
+
+	configFilePath := filepath.Join(homeDir, ".tmail", "config.toml")
+
+	if _, err := os.Stat(configFilePath); err != nil {
+		if os.IsNotExist(err) {
+			return nil, fmt.Errorf("loading config file from %s: %w\n", configFilePath, ErrConfigNotFound)
+		} else {
+			return nil, fmt.Errorf("loading config file from %s: %w\n", configFilePath, err)
+		}
+	}
+
+	_, err = toml.DecodeFile(configFilePath, &config)
+
+	if err != nil {
+		return nil, fmt.Errorf("loading config file from %s: %w\n", configFilePath, ErrConfigNotFound)
 	}
 	return &config, nil
 }
